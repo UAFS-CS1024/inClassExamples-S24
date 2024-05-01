4e63106a31a3bf19d80d3e7f4dc0cc2ec715b59c
John Hightower <john.hightower@uafs.edu>
2024-05-01 12:52:09 -0500

    Update

---
+++ inClassApr29/inClassApr29/HashNode.java
@@ -0,0 +1,13 @@
+package inClassApr29;
+
+public class HashNode {
+	public String key;
+	public String value;
+	
+	public HashNode() {}
+	
+	public HashNode(String key, String value) {
+		this.key=key;
+		this.value=value;
+	}
+}
--- inClassApr29/inClassApr29/OurHashTable.java
+++ inClassApr29/inClassApr29/OurHashTable.java
@@ -2,12 +2,12 @@ package inClassApr29;
 
 public class OurHashTable {
 	
-	private String[] hashTable;
+	private HashNode[] hashTable;
 	private int size;
 	
 	public OurHashTable(int size) {
 		this.size = size;
-		hashTable = new String[this.size];
+		hashTable = new HashNode[this.size];
 	}
 	
 	public int getHash(String key, int size) {
@@ -25,31 +25,66 @@ public class OurHashTable {
 	}
 	
 	public String get(String key) {
+		String value=null;
+		boolean notFound=true;
 		int hashkey = getHash(key,this.size);
-		return this.hashTable[hashkey];
+		int index = hashkey;
+		
+		//*** Find Node ****
+		while(notFound) {
+			if(this.hashTable[index]!=null) {
+				if(this.hashTable[index].key.equals(key)) {
+					notFound=false;
+					value = this.hashTable[index].value;
+					break;
+				}	
+			}
+			index++;
+			if(index==this.size) {
+				index=0;
+			}
+			if(index == hashkey) {	//** Key not in  HashTable
+				notFound=false;
+			}
+		}	
+		return value;
 	}
 	
-	public int put(String key, String value) {
+	public String put(String key, String value) {
+		boolean notInserted=true;
+		HashNode newNode = new HashNode(key,value);
 		int hashkey = getHash(key,this.size);
-		this.hashTable[hashkey] = value;
-		return hashkey;
+		int index = hashkey;
+		
+		//*** Insert Node ****
+		while(notInserted) {
+			if(this.hashTable[index]==null) {
+				hashTable[index] = newNode;
+				notInserted=false;
+			}else {
+				index++;
+				if(index==this.size) {
+					index=0;
+				}
+			}
+		}	
+		return hashkey + "->" + index;
 	}
 
 	public static void main(String[] args) {
-		OurHashTable htable = new OurHashTable(100);
+		OurHashTable htable = new OurHashTable(5);
 		
-		System.out.printf("%d\n", htable.put("car","Honda"));
-		System.out.printf("%d\n", htable.put("dog","Buddy"));
-		System.out.printf("%d\n", htable.put("instructor","Me"));
-		System.out.printf("%d\n", htable.put("course","Programming II"));
-		System.out.printf("%d\n", htable.put("9","Collision"));
+		System.out.printf("%s\n", htable.put("car","Honda"));
+		System.out.printf("%s\n", htable.put("dog","Buddy"));
+		System.out.printf("%s\n", htable.put("instructor","Me"));
+		System.out.printf("%s\n", htable.put("course","Programming II"));
+		System.out.printf("%s\n", htable.put("9","Collision"));
 		
 		System.out.printf("%s\n", htable.get("car"));
 		System.out.printf("%s\n", htable.get("dog"));
 		System.out.printf("%s\n", htable.get("instructor"));
 		System.out.printf("%s\n", htable.get("course"));
 		System.out.printf("%s\n", htable.get("9"));
-
 	}
 
 }
